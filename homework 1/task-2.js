60fd4a12355ee36636c20f983e0d2913983e1991
Victoria232323 <105664847+Victoria232323@users.noreply.github.com>
2023-03-13 18:10:06 +0000

    update

--- homework 1/task-2.js
+++ homework 1/task-2.js
@@ -30,15 +30,40 @@ let isAdult = confirm("are you an adult?");
 console.log(isAdult);
 
 //task 6
-let name;
+let myName;
 let surname;
 let group;
 let yearOrBirth;
 let isSingle = true;
+let r = null;
+let x = undefined;
 
-name = "Victoria";
+myName = "Victoria";
 surname = "Cherepiy";
 group = 1;
 yearOrBirth = 2005;
 
-console.log()
+console.log(group);
+console.log(yearOrBirth);
+console.log(isSingle);
+console.log(myName);
+console.log(surname);
+console.log(typeof(r));
+console.log(typeof(x));
+
+//task 7
+let email = prompt("Login?");
+let password = prompt("Password?");
+alert("Dear User, your email is " + email + "your password is " + password);
+
+//task 8
+let secondInMinute = 60;
+let minuteInHour = 60;
+let secondInHour = secondInMinute * minuteInHour;
+let hourInDay = 24;
+let secondInDay = secondInHour * hourInDay;
+let dayInMonth = 30;
+let secondInMonth = secondInDay * dayInMonth;
+console.log(secondInHour);
+console.log(secondInDay);
+console.log(secondInMonth);
