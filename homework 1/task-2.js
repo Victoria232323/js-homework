b3b983d0f008adf8108239c3add41b1b5ac7b354
Victoria232323 <105664847+Victoria232323@users.noreply.github.com>
2023-03-13 13:58:51 +0000

    a

---
+++ homework 1/task-2.js
@@ -0,0 +1,44 @@
+//task 2
+console.log("Cherepiy");
+
+//task 3
+let season;
+let day;
+
+season = "spring";
+day = 13;
+
+alert(season);
+alert(day);
+
+season = day;
+
+alert(season);
+alert(day);
+
+//task 4
+let obj = {
+    "String" : "hello",
+    "Number" : 512,
+    "Boolean": true,
+    "Undefined" : undefined,
+    "Null" : null
+}
+
+//task 5
+let isAdult = confirm("are you an adult?");
+console.log(isAdult);
+
+//task 6
+let name;
+let surname;
+let group;
+let yearOrBirth;
+let isSingle = true;
+
+name = "Victoria";
+surname = "Cherepiy";
+group = 1;
+yearOrBirth = 2005;
+
+console.log()
