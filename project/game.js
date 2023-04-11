b354cc9a2c2cb8112604fbfd22246aa5e5df3e55
Victoria232323 <105664847+Victoria232323@users.noreply.github.com>
2023-04-11 15:47:01 +0000

    added move

--- project/game.js
+++ project/game.js
@@ -2,29 +2,33 @@ const cookieValue = document.cookie.split('; ').find(row => row.startsWith('valu
 const difficulty = cookieValue ? decodeURIComponent(cookieValue.split('=')[1]) : null;
 
 function setTargets(targets){
-    const targetsField = document.querySelector('.game');
-    const target = '<img src="img/target.png" class="target">';
-    
+    const allTargets = document.querySelector('.game');
     for (let i = 0; i < targets; i++) {
-        const movingImageContainer = document.createElement('div');
-        movingImageContainer.id = 'movingImageContainer';
-        movingImageContainer.classList.add('game');
-        document.body.appendChild(movingImageContainer);
-
-        const movingImage = document.createElement('img');
-        movingImage.src = 'img/target.png';
-        movingImage.classList.add('target');
-        movingImageContainer.appendChild(movingImage);
-
-        // Установка начальных координат и стилей
-        const screenWidth = window.innerWidth;
-        const screenHeight = window.innerHeight;
-        const initialX = Math.random() * (screenWidth - movingImageContainer.offsetWidth);
-        const initialY = Math.random() * (screenHeight - movingImageContainer.offsetHeight);
-        const initialTransform = `translate(${initialX}px, ${initialY}px)`;
-        movingImageContainer.style.transform = initialTransform;
+        allTargets.innerHTML += '<div class = "targetDiv"><img src="img/target.png" class="target"><div>';
     }
     
+}
+
+function moveTargets(speed){
+    const targets = document.querySelectorAll('.target');
+
+    let direction = 1; 
+    let distance = 0; 
+
+    setInterval(() => {
+        targets.forEach(target => {
+            
+            distance += direction * speed;
+
+            
+            if (distance >= window.innerWidth - target.offsetWidth || distance <= 0) {
+                direction *= -1;
+            }
+
+            
+            target.style.transform = `translateX(${distance}px)`;
+        });
+    }, 1000 / 60);
 }
 
 function setLives(lives){
@@ -37,7 +41,33 @@ function setLives(lives){
 function game(targets, lives, speed){
     setLives(lives);
     setTargets(targets);
-    let points = 0;
+    moveTargets(speed);
+
+    const allTargets = document.querySelectorAll('.target');
+    let points = 0; 
+    
+    allTargets.forEach(target => {
+        target.addEventListener('click', (event) => {
+            points++; 
+            const emptyDiv = document.createElement('div');
+            emptyDiv.className = 'empty'; 
+            event.target.parentNode.replaceChild(emptyDiv, event.target); 
+            return false;
+        });
+    });
+
+    const field = document.querySelector('.game');
+    const allLives = document.querySelectorAll('.lives');
+    
+    field.addEventListener('click', () => {
+        if (lives > 0 && allLives.length > 0) { 
+            lives--; 
+            const lastLife = allLives[allLives.length - 1]; 
+            lastLife.parentNode.removeChild(lastLife); 
+        }
+    });
+    
+
     if(lives === 0) return false;
     setTimeout(()=> {
         if(points !== targets){
@@ -47,9 +77,9 @@ function game(targets, lives, speed){
 }
 
 if(difficulty === 'easy'){
-    game(3, 5, 2);
+    game(3, 5, 0.7);
 } else if(difficulty === 'normal'){
-    game(5, 3, 3);
+    game(5, 3, 0.8);
 } else{
-    game(8, 1, 5);
+    game(8, 1, 0.9);
 }
